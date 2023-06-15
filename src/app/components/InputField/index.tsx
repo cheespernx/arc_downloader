6f4efa94703b6eaa507cabf56a97cc0a661a948c
Victor Carvalho <contato@cheesper.com>
2023-06-15 13:41:57 -0400

    WIP: Added new API Requests

--- src/app/components/InputField/index.tsx
+++ src/app/components/InputField/index.tsx
@@ -2,14 +2,21 @@
 import { useCallback, useEffect, useState } from 'react';
 import './styles.scss'
 import TypeSwitch from '../Switch';
+import axios from 'axios';
 
-type URLType = 'Instagram' | 'Youtube' | null
+const URLTypes = {
+  Youtube: 'Youtube',
+  Instagram: 'Instagram',
+  null: null
+} as const
+
+type URLTypes = typeof URLTypes[keyof typeof URLTypes]
 
 const InputSearchField = () => {
 
   const [ isValidURL, setIsValidURL ] = useState<boolean>(false);
   const [ inputValue, SetInputValue ] = useState<string>('');
-  const [ URLType, SetURLType ] = useState<URLType>(null);
+  const [ URLType, SetURLType ] = useState<URLTypes>(null);
   const [ switchHidden, SetSwitchHidden ] = useState<boolean>(false);
 
   const validateURL = useCallback( (URL: string) => {
@@ -18,16 +25,18 @@ const InputSearchField = () => {
     if (URLRegex.test(URL)){
       if (
         (URL.includes('https://www.instagram.com/p/') && URL.length > 29) ||
-        (URL.includes('https://instagram.com/p/') && URL.length > 25)
+        (URL.includes('https://instagram.com/p/') && URL.length > 25) ||
+        (URL.includes('https://www.instagram.com/reel/') && URL.length > 28) ||
+        (URL.includes('https://instagram.com/reel/') && URL.length > 28)
       ) {
-        SetURLType('Instagram');
+        SetURLType(URLTypes.Instagram);
         setIsValidURL(true)
       } else if (
         (URL.includes('https://youtube.com/watch?v=') && URL.length > 29) ||
         (URL.includes('https://m.youtube.com/watch?v=')&& URL.length > 31) ||
         (URL.includes('https://www.youtube.com/watch?v=')&& URL.length > 33)
       ){
-        SetURLType('Youtube');
+        SetURLType(URLTypes.Youtube);
         setIsValidURL(true)
       } else {
         SetURLType(null);
@@ -49,15 +58,51 @@ const InputSearchField = () => {
   }, [inputValue, validateURL])
 
   useEffect(() => {
-    if (URLType === 'Youtube') {
+    if (URLType === URLTypes.Youtube) {
       SetSwitchHidden(false);
     } else {
       SetSwitchHidden(true);
     }
   }, [URLType])
 
-  const startDownloadProcess = () => {
-    console.log(inputValue);
+  const startDownloadProcess = async () => {
+
+    switch (URLType) {
+      case URLTypes.Youtube:
+        console.log('Youtube');
+        break;
+      case URLTypes.Instagram:
+        const _urlParsed: string[] = inputValue.split('.com/');
+        const postCode: string = _urlParsed[1].split('/')[1];
+
+        const encodedParams = new URLSearchParams();
+        encodedParams.set('url', postCode);
+
+        const options = {
+          method: 'GET',
+          url: process.env.NEXT_PUBLIC_RAPID_API_URL,
+          params: {
+            short_code: postCode
+          },
+          headers: {
+            'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
+            'X-RapidAPI-Host': process.env.NEXT_PUBLIC_RAPID_API_HOST
+          }
+        };
+
+        try {
+          const response = await axios.request(options);
+          console.log(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+
+        break;
+    
+      default:
+        break;
+    }
+    
   }
 
   return(
